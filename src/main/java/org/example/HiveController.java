e68277687f9ee63359584bfce673b6996d87f125
Eugene <oeugene286@gmail.com>
2026-03-30 02:58:14 +0000

    Take Finale

--- src/main/java/org/example/HiveController.java
+++ src/main/java/org/example/HiveController.java
@@ -220,7 +220,7 @@ public class HiveController {
         } else {
             try {
                 RestTemplate restTemplate = new RestTemplate();
-                String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=" + apiKey;
+                String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=" + apiKey;
                 String safeQuestion = questionText.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", " ").replace("\r", " ");
 
                 String requestBody = "{"
